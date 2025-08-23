d57bcca1eadba69e05cff243129a419e02f0688d
S. M. ASHFAQ HOSSAIN <ashfaqhossain830@gmail.com>
2025-08-23 11:29:15 +0600

    Create Area of rectangle.cpp

---
+++ Area of rectangle.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int w,h,area;
+    cin>>w>>h;
+    area=w*h;
+    cout<<w<<" "<<h<<"\n";
+    cout<<area<<endl;
+       
+    return 0;
+}
